6d3bc8076f9462615c235d274a7843449458deb2
tc <tomasz.czana@gmail.com>
2018-06-27 18:57:14 +0200

    remove redux imports

--- client/index.jsx
+++ client/index.jsx
@@ -2,14 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Game from './components/Game'
 
-import { store } from './store'
-import { Provider } from 'react-redux'
-
 import './styles/game'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Game />
-  </Provider>,
-  document.getElementById('game')
-)
+ReactDOM.render(<Game />, document.getElementById('game'))
