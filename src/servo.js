57efbcb44bca6b4723c71ba41ec070c945c13283
czana <tomasz.czana@gmail.com>
2018-07-09 23:38:14 +0200

    servo tweaks

--- src/servo.js
+++ src/servo.js
@@ -3,17 +3,17 @@ import { Gpio } from 'onoff'
 class Servo {
   constructor(pin, delay) {
     this.servo = new Gpio(pin, 'out')
-    this.servo.writeSync(Gpio.LOW)
+    this.servo.writeSync(Gpio.HIGH)
     this.delay = delay
   }
 
   move() {
-    this.servo.writeSync(Gpio.HIGH)
+    this.servo.writeSync(Gpio.LOW)
 
     setTimeout(() => {
-      this.servo.writeSync(Gpio.LOW)
+      this.servo.writeSync(Gpio.HIGH)
     }, this.delay)
   }
 }
 
-export default new Servo(17, 2000)
+export default new Servo(17, 4000)
